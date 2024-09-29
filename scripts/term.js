2deebabc4a07107417e2c78592fa4ce799a79cdc
aklevans <alexklevans@gmail.com>
2024-09-29 14:52:30 -0400

    terminal

---
+++ scripts/term.js
@@ -0,0 +1,12 @@
+console.log("testing")
+$("#term").terminal({
+    hello: function(what) {
+        this.echo('Hello, ' + what +
+                  '. Wellcome to this terminal.');
+    },
+    cat: function() {
+        this.echo($('<img src="https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg">'));
+    }
+}, {
+    greetings: 'Squibbly toilet'
+});
