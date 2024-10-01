7e2c89992326d9fa3f21b64d3ad1e3c0c10b9110
aklevans <alexklevans@gmail.com>
2024-10-01 18:00:32 -0400

    dfdsfds

--- scripts/term.js
+++ scripts/term.js
@@ -93,6 +93,7 @@ $("#term").terminal({
         file = lookUp(path);
         if(file.type == downloadable_type) {
             let link = document.createElement("a");
+            link.download="seal.wav";
             link.href = file.path;
             link.click();
             link.remove();
